41f3dbb82c3b9ca9f9ab4f022143580e430b55d1
Carol Andrade <78247893+carol-andradech@users.noreply.github.com>
2024-03-20 20:30:53 -0300

    pedidos sem att

--- src/Components/Pedidos/Pedidos.tsx
+++ src/Components/Pedidos/Pedidos.tsx
@@ -1,4 +1,3 @@
-// Pedidos.js
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import React, { useEffect, useState } from "react";
@@ -13,22 +12,29 @@ import { useProduto } from "../Produtos/useLocalStorageProduto";
 Modal.setAppElement("#root");
 
 const schema = yup.object().shape({
-  nome: yup.string().required("Nome é obrigatório"),
-  preco: yup.number().required("Preço é obrigatório"),
-  descricao: yup.string().required("Descrição é obrigatória"),
+  nome_cliente: yup.string().required("Nome do cliente é obrigatório"),
+  produtos: yup.array().of(
+    yup.object().shape({
+      nome: yup.string().required("Nome do produto é obrigatório"),
+      quantidade: yup
+        .number()
+        .min(0, "Quantidade mínima é 0") // Agora permitimos 0 para produtos removidos
+        .required("Quantidade é obrigatória"),
+      preco: yup.number().required("Preço do produto é obrigatório"),
+      total: yup.number().required("Total do produto é obrigatório"),
+    })
+  ),
 });
+
 export default function Pedidos() {
   const [pedidos, setPedidos] = useState([]);
-  const [selectedPedido, setSelectedPedido] = useState(null);
   const [newPedidoModalOpen, setNewPedidoModalOpen] = useState(false);
-  const [pedidoDetailsModalOpen, setPedidoDetailsModalOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
-  const [quantidade, setQuantidade] = useState(0); // Estado para a quantidade de produtos no pedido
-  const { createPedido, deletePedido, pedido } = usePedido();
-  const { produto } = useProduto();
-  const { clients } = useClient();
   const [selectedClient, setSelectedClient] = useState(null);
-
+  const [totalProdutos, setTotalProdutos] = useState(0);
+  const [totalPreco, setTotalPreco] = useState(0);
+  const { clients } = useClient();
+  const { produto } = useProduto();
   const {
     register,
     handleSubmit,
@@ -38,56 +44,91 @@ export default function Pedidos() {
     resolver: yupResolver(schema),
   });
 
-  const handlePedidoClick = (pedido) => {
-    setSelectedPedido(pedido);
-    setPedidoDetailsModalOpen(true);
-  };
-
   const handleDeleteAllPedidos = () => {
-    pedidos.forEach((pedido) => {
-      deletePedido(pedido.id);
-    });
+    setPedidos([]);
+    setTotalProdutos(0);
+    setTotalPreco(0);
   };
 
-  const onSubmitNewPedido = (data) => {
-    createPedido(data);
-    setNewPedidoModalOpen(false);
-    reset();
+  const onSubmitNewPedido = (formData) => {
+    if (selectedClient) {
+      const pedidoData = {
+        cliente: selectedClient.nome,
+        produtos: formData.produtos.map((produto) => ({
+          nome: produto.nome,
+          quantidade: produto.quantidade,
+          total: produto.quantidade * produto.preco,
+        })),
+      };
+      setPedidos([pedidoData]); // Adiciona o novo pedido à lista de pedidos
+      setNewPedidoModalOpen(false);
+      reset();
+      setSelectedClient(null);
+      setSearchTerm("");
+    }
   };
 
-  const handleSearchInputChange = (event) => {
-    setSearchTerm(event.target.value);
+  const handleAddToPedido = (produto) => {
+    if (selectedClient && produto) {
+      const preco = parseFloat(produto.preco.replace(",", "."));
+      const existingPedidoIndex = pedidos.findIndex(
+        (pedido) => pedido.nome === produto.nome
+      );
+      if (existingPedidoIndex !== -1) {
+        const updatedPedidos = [...pedidos];
+        updatedPedidos[existingPedidoIndex] = {
+          ...updatedPedidos[existingPedidoIndex],
+          quantidade: updatedPedidos[existingPedidoIndex].quantidade + 1,
+          total: updatedPedidos[existingPedidoIndex].total + preco,
+        };
+        setPedidos(updatedPedidos);
+      } else {
+        const novoPedido = {
+          nome: produto.nome,
+          quantidade: 1,
+          preco: preco,
+          total: preco,
+        };
+        setPedidos([...pedidos, novoPedido]);
+      }
+      setTotalProdutos(totalProdutos + 1);
+      setTotalPreco(totalPreco + preco);
+    }
+  };
+
+  const handleRemoveFromPedido = (produto) => {
+    if (produto) {
+      const preco = parseFloat(produto.preco.replace(",", "."));
+      const existingPedidoIndex = pedidos.findIndex(
+        (pedido) => pedido.nome === produto.nome
+      );
+      if (existingPedidoIndex !== -1) {
+        const updatedPedidos = [...pedidos];
+        if (updatedPedidos[existingPedidoIndex].quantidade > 1) {
+          updatedPedidos[existingPedidoIndex] = {
+            ...updatedPedidos[existingPedidoIndex],
+            quantidade: updatedPedidos[existingPedidoIndex].quantidade - 1,
+            total: updatedPedidos[existingPedidoIndex].total - preco,
+          };
+        } else {
+          updatedPedidos.splice(existingPedidoIndex, 1);
+        }
+        setPedidos(updatedPedidos);
+        setTotalProdutos(totalProdutos - 1);
+        setTotalPreco(totalPreco - preco);
+      }
+    }
   };
 
   const handleClientSelect = (client) => {
     setSelectedClient(client);
+    setPedidos([]);
+    setTotalProdutos(0);
+    setTotalPreco(0);
   };
 
-  const handleAddToPedido = (productId) => {
-    console.log("Produto adicionado ao pedido:", productId);
-    setQuantidade(quantidade + 1);
-  };
-
-  const handleRemoveFromPedido = (productId) => {
-    console.log("Produto removido do pedido:", productId);
-    if (quantidade > 0) {
-      setQuantidade(quantidade - 1);
-    }
-  };
-
-  const handleFormSubmit = (data) => {
-    // Lógica para lidar com o envio do formulário
-    onSubmitNewPedido(data);
-  };
-
-  const handleAddClick = () => {
-    setQuantidade(quantidade + 1);
-  };
-
-  const handleRemoveClick = () => {
-    if (quantidade > 0) {
-      setQuantidade(quantidade - 1);
-    }
+  const handleSearchInputChange = (event) => {
+    setSearchTerm(event.target.value);
   };
   return (
     <>
@@ -118,7 +159,7 @@ export default function Pedidos() {
         className="modal-content"
       >
         <h2>Cadastro do Pedido</h2>
-        <form onSubmit={handleSubmit(handleFormSubmit)}>
+        <form onSubmit={handleSubmit(onSubmitNewPedido)}>
           <div className="dropdown">
             <select
               name="cliente"
@@ -142,27 +183,75 @@ export default function Pedidos() {
               onChange={handleSearchInputChange}
             />
           </div>
-
           <div className="produto-list-pedido">
-            {produto.map((produto) => (
-              <div className="produto-item-pedido" key={produto.id}>
-                <img src={produto.imagem} alt={produto.nome} />
-                <div className="produto-details-pedido">
-                  <h3>{produto.nome}</h3>
-                  <p>Preço: R${produto.preco}</p>
-                  <div className="produto-actions-pedido">
-                    <button onClick={handleAddClick}>+ Adicionar</button>
-                    <span>{quantidade}</span>
-                    <button onClick={handleRemoveClick}>- Remover</button>
+            {produto.map((produto) => {
+              const pedido = pedidos.find(
+                (pedido) => pedido.nome === produto.nome
+              );
+              const quantidade = pedido ? pedido.quantidade : 0;
+              return (
+                <div className="produto-item-pedido" key={produto.id}>
+                  <img src={produto.imagem} alt={produto.nome} />
+                  <div className="produto-details-pedido">
+                    <h3>{produto.nome}</h3>
+                    <p>Preço: R${produto.preco}</p>
+                    <div className="produto-actions-pedido">
+                      <button
+                        type="button"
+                        onClick={() => handleAddToPedido(produto)}
+                      >
+                        +
+                      </button>
+                      <span>{quantidade}</span>
+                      <button
+                        type="button"
+                        onClick={() => handleRemoveFromPedido(produto)}
+                      >
+                        -
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              );
+            })}
           </div>
-
+          <div>
+            <p>
+              Nome do Cliente:{" "}
+              {selectedClient
+                ? selectedClient.nome
+                : "Nenhum cliente selecionado"}
+            </p>
+            <p>
+              Quantidade de Produtos:{" "}
+              {pedidos.reduce((acc, pedido) => acc + pedido.quantidade, 0)}
+            </p>
+            <p>Valor Total do Pedido: R${totalPreco}</p>
+          </div>
           <button type="submit">Salvar</button>
         </form>
       </Modal>
+
+      <div className="pedidos-list">
+        {pedidos.length > 0 ? (
+          pedidos.map((pedido, index) => (
+            <div className="pedido-item" key={index}>
+              <img src="src\assets\empty.jpg" alt={pedido.nome} />
+              <div className="pedido-left">
+                <h3>{pedido.cliente}</h3>{" "}
+                <span> Qtd. produtos: {pedido.quantidade}</span>{" "}
+              </div>
+              <div className="pedido-right">
+                {/* Adicionando logs para depurar */}
+                {console.log("pedido.total:", pedido.total)}
+                <p> R${pedido.total}</p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>Nenhum pedido cadastrado ainda.</p>
+        )}
+      </div>
     </>
   );
 }
